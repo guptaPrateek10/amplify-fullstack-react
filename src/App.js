549e8fb7a936cbc77537abeb51eba9f230189ea2
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-12-19 18:16:41 +0530

    Auth added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Amplify } from "aws-amplify";
+import "./App.css"
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
-function App() {
+export default function App() {
   return (
-    <div >
-     <p>AWS Amplify fullstack app Hosted</p>
+    <div className="App" >
+      <>
+        <Authenticator>
+          {({ signOut, user }) => (
+            <main>
+              <h1>Hello {user.username}</h1>
+              <button onClick={signOut}>Sign out</button>
+            </main>
+          )}
+        </Authenticator>
+      </>
     </div>
   );
 }
-
-export default App;
